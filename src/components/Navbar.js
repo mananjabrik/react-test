ed82a507e39ef0c02305275212078800549d5687
mananjabrik <manan.jabrik@gmail.com>
2020-11-23 22:03:55 +0700

    edit css media and logic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,8 +40,16 @@ function Navbar() {
                     </ul>
                 </div>
             </nav>
+
         </>
     )
 }
-
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 export default Navbar
